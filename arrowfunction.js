e1135712fd4ff703ed9ebb8074e4fcaa3d966310
designMoreWeb <divystolia@gmail.com>
2017-01-21 15:33:28 -0500

    Create an arrow function file and added some examples of functions in es5 vs. es6

---
+++ arrowfunction.js
@@ -0,0 +1,13 @@
+//ES5
+var hello = function(hello, namePerson){
+   return hello+ namePerson;
+}
+
+
+//ES6
+
+var arrowHello = (hello,namePerson) => hello + namePerson;
+
+ // another example
+var hi = message => "Hi";
+
